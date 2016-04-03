58cf206f6e60283c9b373ff2e5031f0c007815d7
wduntak <wduntak@gmail.com>
2016-04-03 16:44:18 -0400

    Setup nested routes with this.props.children

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,15 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
+const Greeting = () => {
+	return <div>Hey There!</div>;
+}
+
+
 export default (
-	<Route path="/" component={App} />
+	<Route path="/" component={App}>
+		<Route path="greet" component={Greeting} />
+		<Route path="greet1" component={Greeting} />
+		<Route path="greet2" component={Greeting} />
+	</Route>
 )
