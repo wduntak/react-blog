6a0be948a886762e475e257b904bdd0c64994536
wduntak <wduntak@gmail.com>
2016-04-03 17:00:13 -0400

    Remove nested route examples and add IndexRoute

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,10 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-
-const Greeting = () => {
-	return <div>Hey There!</div>;
-}
-
+import PostsIndex from './components/posts_index'
 
 export default (
 	<Route path="/" component={App}>
-		<Route path="greet" component={Greeting} />
-		<Route path="greet1" component={Greeting} />
-		<Route path="greet2" component={Greeting} />
+		<IndexRoute component={PostsIndex} />
 	</Route>
-)
+);
