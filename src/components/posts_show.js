6c858f300edeaaf3d7d0c3c66b45cc30f5a4f8e2
Wangdu Duntak <wduntak@lp-229.kobo.corp>
2016-04-04 10:48:01 -0400

    Show post, added back btn and delete btn functionality

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,8 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
+	onDelete() {
+		this.props.deletePost(this.props.params.id)
+			.then(() => { 
+				this.context.router.push('/');
+			})
+	};
+
 	componentWillMount() {
 		this.props.fetchPost(this.props.params.id);
 	}
@@ -16,10 +28,17 @@ class PostsShow extends Component {
 
 		return (
 			<div>
+				<Link to="/">Back to Index</Link>
+				<button 
+					className="btn btn-danger pull-xs-right"
+					onClick={this.onDelete.bind(this)}>
+					Delete Post
+				</button>
 				<h3>{post.title}</h3>
 				<h6>Categories: {post.categories}</h6>
 				<p>{post.content}</p>
 			</div>
+
 		)
 	}
 }
@@ -28,4 +47,4 @@ function mapStateToProps(state) {
 	return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
