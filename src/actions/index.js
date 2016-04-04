60ae11bcf834187bca4129550a2487860bca85ee
Wangdu Duntak <wduntak@lp-229.kobo.corp>
2016-04-04 10:22:58 -0400

    Setup redux to handle data and have action to get post data for show post page

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
+
 const ROOT_URL = `http://reduxblog.herokuapp.com/api`;
 const API_KEY = `?key=asdn8s9ad9asd2al`;
 
@@ -18,5 +20,14 @@ export function createPost(props) {
 	return {
 		type: CREATE_POST,
 		payload: request
-	}
+	};
+}
+
+export function fetchPost(id) {
+	const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+	return {
+		type: FETCH_POST,
+		payload: request
+	};
 }
